f7bf5422c27535e1bb25fbd862797012cc948c57
Domerck <qdomerck@gmail.com>
2022-03-22 16:52:06 +0800

    fixed bugs

--- main.js
+++ main.js
@@ -458,6 +458,21 @@ function newaddRowHandlers() {
 // end: attribute query
 
 
+//Modal
+
+// Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
@@ -469,7 +484,6 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
   }  
-
 
 // start: attribute query
 
